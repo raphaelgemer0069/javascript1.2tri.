3894777790b8e3281011e971452422a5fa5a16c9
raphaelgemer0069 <raphael.correa.moraes@escola.pr.gov.br>
2026-03-20 07:54:03 -0300

    update

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -7,3 +7,14 @@ let numero = 30
 let decimal =20.30
 // string
 let nome = "não sei, você não sabe"
+let nome1 = "NÂO sei o que é certo ou errado vou buscar resposta"
+console.log(nome === nome1);// false
+const inputMinusculo = nome1.tolovwerCase();
+console.log(nome === inputMinusculo);
+const senha = "minhasenha123"
+console.log(senha.length)// 13 caracteres
+let nv= true;
+let nz = false;
+if (nv!==nz){
+    console.log(`nv é diferente,pois ele é${nv}`)
+}
