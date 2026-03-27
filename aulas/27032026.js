13a3fdbfb24975d853d8055fe7d6dcea4f3a8b5a
raphaelgemer0069 <raphael.correa.moraes@escola.pr.gov.br>
2026-03-27 07:56:41 -0300

    updeta

---
+++ aulas/27032026.js
@@ -0,0 +1,14 @@
+// Criar Array
+
+const FilaNumber = [1,2,3,4,5,6];
+const fileStringAmigos = ["Jão Paulo","Jão Vitor","Gustavo","Miguel","Renan","José","Raphael","Eu"];
+
+function LerAmigos(filaStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length} amigos`)
+    for(let i = 0; i < filaStringAmigos.length;i++){
+console.log(filaStringAmigos[i] )
+if(filaStringAmigos [i]=== "Eu"){
+    console.log(`Index do seu melhor amigosé ${i}`)
+}
+    }
+}
